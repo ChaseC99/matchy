d61a09a87ff33948235d51a211f84d8f03b471db
Chase Carnaroli <chasecarnaroli@gmail.com>
2020-09-19 14:02:59 -0700

    Initial commit for py file

---
+++ matchy.py
@@ -0,0 +1,12 @@
+# Matchy
+
+# Imports
+import slack
+
+# Get Channel Members
+#   Returns a list of slack ids for the users in the channel
+def get_channel_members(channel: str) -> [str]:
+    pass
+
+if __name__ == "__main__":
+    pass
